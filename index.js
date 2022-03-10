d20bbf05876dc89e1039b61e95825ca2e47e4a01
Nilton <niltonroese@gmail.com>
2022-03-10 14:38:03 +1000

    Done with it

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    Name: 'Nilton', 
+    streetAddress: '94 Amarina Ave'
+}
+
+function updateEmployeeWithKeyAndValue(employee, name, value) {
+    const newEmployee = {...employee};
+    newEmployee[name] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, value) {
+    employee[name] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, name) {
+    const clonedEmployee = {...employee};
+    delete clonedEmployee.name;
+    return clonedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name;
+    return employee;
+}
